d945e5f0243d22941561637c0a3edeae2688d28a
foymikek <foymikek@gmail.com>
2025-03-08 16:36:39 -0700

    add passing Registrant initialize

---
+++ spec/registrant_spec.rb
@@ -0,0 +1,20 @@
+require '../spec/spec_helper'
+
+RSpec.describe Registrant do
+  before(:each) do
+    @registrant_1 = Registrant.new('Bruce', 18, true )
+    @registrant_2 = Registrant.new('Penny', 15)
+  end
+  describe "#initialize" do
+    it 'can intitialize' do
+      expect(@registrant_1).to be_an_instance_of Registrant
+      expect(@registrant_1.name).to eq "Bruce"
+      expect(@registrant_1.age).to eq 18
+      expect(@registrant_1.permit).to eq true
+      expect(@registrant_2.name).to eq "Penny"
+      expect(@registrant_2.age).to eq 15
+      expect(@registrant_2.permit).to eq false
+    end
+  end
+end
+
