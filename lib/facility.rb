86458de8a6fd388e523560ad3868dfa116944529
foymikek <foymikek@gmail.com>
2025-03-11 16:57:01 -0600

    add passing facility can register vehicle

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,27 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(args)
     @name = args[:name]
     @address = args[:address]
     @phone = args[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle
+    vehicle.registration_date = Date.today
+    if vehicle.antique?
+      vehicle.plate_type = :antique
+    elsif vehicle.electric_vehicle?
+      vehicle.plate_type = :ev
+    else 
+      vehicle.plate_type = :regular
+    end
   end
 end
